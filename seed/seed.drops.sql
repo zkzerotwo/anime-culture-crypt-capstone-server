7d2b07ad53f9cf9d5f9d827a53af3120f5cbf700
Christopher 'ZK' Hal <zkzerotwo@gmail.com>
2021-03-27 15:58:08 -0500

    seeds complete

---
+++ seed/seed.drops.sql
@@ -0,0 +1,12 @@
+TRUNCATE drops RESTART IDENTITY CASCADE;
+
+INSERT INTO drops 
+(series_id, drop_description, lootbox_id)
+
+VALUES 
+('1234', 'A lovely advnture of two lovers star crossed through time.', 1),
+('534', 'A lovely advnture of two lovers star crossed through time.', 1),
+('25', 'A lovely advnture of two lovers star crossed through time.', 1),
+('98', 'A lovely advnture of two lovers star crossed through time.', 1),
+('7647', 'A lovely advnture of two lovers star crossed through time.', 1),
+('958', 'A lovely advnture of two lovers star crossed through time.', 1);
