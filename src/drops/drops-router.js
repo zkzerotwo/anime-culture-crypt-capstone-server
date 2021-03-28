575afb56123b810cf7c4bbd961228d06068b32b2
Christopher 'ZK' Hal <zkzerotwo@gmail.com>
2021-03-27 20:41:22 -0500

    fixed drop router

--- src/drops/drops-router.js
+++ src/drops/drops-router.js
@@ -1,47 +1,160 @@
-const DropService = {
-    //relevant
-    getDrops(db) {
-        return db
-            .select('*')
-            .from('drop')
-    },
-    getDropById(db, drop_id) {
-        return db
-            .select('*')
-            .from('drop')
-            .where('drop.id', drop_id)
-            .first()
-    },
-    //relevant
-    insertDrop(db, newDrop) {
-        return db
-            .insert(newDrop)
-            .into('drop')
-            .returning('*')
-            .then(rows => {
-                return rows[0]
-            })
-    },
-    //relevant
-    updateDrop(db, drop_id, newDrop) {
-        return db('drop')
-            .update(newDrop, returning = true)
-            .where({
-                id: drop_id
-            })
-            .returning('*')
-            .then(rows => {
-                return rows[0]
-            })
-    },
-    //relevant
-    deleteDrop(db, drop_id) {
-        return db('drop')
-            .delete()
-            .where({
-                'id': drop_id
-            })
-    }
-}
+const path = require('path')
+const express = require('express')
+const xss = require('xss')
+const DropsService = require('./drops-service')
 
-module.exports = DropService
+const dropsRouter = express.Router()
+const jsonParser = express.json()
+
+//filter out the response to avoid showing broken data
+const serializeDrops = drop => ({
+    id: drop.id,
+    title: xss(drop.title),
+    completed: drop.completed
+})
+
+dropsRouter
+    .route('/')
+    //relevant
+    .get((req, res, next) => {
+
+        //connect to the service to get the data
+        DropsService.getDrops(req.app.get('db'))
+            .then(drops => {
+                //map the results to get each one of the objects and serialize them
+                res.json(drops.map(serializeDrops))
+            })
+            .catch(next)
+    })
+    //relevant
+    .post(jsonParser, (req, res, next) => {
+
+        //take the input from the user
+        const {
+            title,
+            completed = false
+        } = req.body
+        const newDrop = {
+            title,
+            completed
+        }
+
+        //validate the input
+        for (const [key, value] of Object.entries(newDrop)) {
+            if (value == null) {
+                //if there is an error show it
+                return res.status(400).json({
+                    error: {
+                        message: `Missing '${key}' in request body`
+                    }
+                })
+            }
+        }
+
+        //save the input in the db
+        DropsService.insertDrop(
+                req.app.get('db'),
+                newDrop
+            )
+            .then(drop => {
+                res
+                //display the 201 status code
+                    .status(201)
+                    //redirect the request to the original url adding the drop id for editing
+                    .location(path.posix.join(req.originalUrl, `/${drops.id}`))
+                    //return the serialized results
+                    .json(serializeDrop(drop))
+            })
+            .catch(next)
+    })
+
+
+dropsRouter
+    .route('/:drop_id')
+    .all((req, res, next) => {
+        if (isNaN(parseInt(req.params.drop_id))) {
+            //if there is an error show it
+            return res.status(404).json({
+                error: {
+                    message: `Invalid id`
+                }
+            })
+        }
+
+        //connect to the service to get the data
+        DrospService.getDropById(
+                req.app.get('db'),
+                req.params.drops_id
+            )
+            .then(drop => {
+                if (!drop) {
+                    //if there is an error show it
+                    return res.status(404).json({
+                        error: {
+                            message: `Drop doesn't exist`
+                        }
+                    })
+                }
+                res.drop = drop
+                next()
+            })
+            .catch(next)
+    })
+    .get((req, res, next) => {
+
+        //get each one of the objects from the results and serialize them
+        res.json(serializeDrop(res.drop))
+    })
+    //relevant
+    .patch(jsonParser, (req, res, next) => {
+
+        //take the input from the user
+        const {
+            title,
+            completed
+        } = req.body
+        const dropToUpdate = {
+            title,
+            completed
+        }
+
+        //validate the input by checking the length of the dropToUpdate object to make sure that we have all the values
+        const numberOfValues = Object.values(dropToUpdate).filter(Boolean).length
+        if (numberOfValues === 0) {
+            //if there is an error show it
+            return res.status(400).json({
+                error: {
+                    message: `Request body must content either 'title' or 'completed'`
+                }
+            })
+        }
+
+        //save the input in the db
+        DropsService.updateDrop(
+                req.app.get('db'),
+                req.params.drop_id,
+                dropToUpdate
+            )
+            .then(updatedDrop => {
+
+                //get each one of the objects from the results and serialize them
+                res.status(200).json(serializeDrop(updatedDrop))
+            })
+            .catch(next)
+    })
+    //relevant
+    .delete((req, res, next) => {
+        DropsService.deleteDrop(
+                req.app.get('db'),
+                req.params.drop_id
+            )
+            .then(numRowsAffected => {
+
+                //check how many rows are effected to figure out if the delete was successful
+                res.status(204).json(numRowsAffected).end()
+            })
+            .catch(next)
+    })
+
+
+module.exports = dropsRouter
