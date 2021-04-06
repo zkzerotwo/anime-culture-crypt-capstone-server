b3ddef1159451cd32573d03ed7e941f083b52f57
Christopher 'ZK' Hal <zkzerotwo@gmail.com>
2021-04-05 21:18:56 -0500

    SSL postgres connection

--- src/server.js
+++ src/server.js
@@ -6,6 +6,9 @@ const {
 } = require('./config')
 const knex = require('knex')
 
+const pg = require('pg');
+pg.defaults.ssl = process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false;
+
 const db = knex({
     client: 'pg',
     connection: DATABASE_URL
